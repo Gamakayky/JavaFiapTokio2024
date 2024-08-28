0f83efce6520f7a3872c5b5e5c92571ddcd43040
Gamakayky <kaykyalexandre18@icloud.com>
2024-08-28 14:19:58 -0300

    atualizacoes e calculadora em java

--- JavaTokio2024/src/logica/aulas/aula04/PrimeiroPrograma.java
+++ JavaTokio2024/src/logica/aulas/aula04/PrimeiroPrograma.java
@@ -19,10 +19,10 @@ public class PrimeiroPrograma {
 		String nome = "kayky";
 		System.out.println(nome);
 		
-		System.out.println("beem-vindo(a), " + nome + "!");
+		System.out.println("beem-vindo(a), " + nome + " ! ");
 		System.out.println(nome + ", você tem " + (idade+idade) + " anos.");
 		
-		System.out.printf("Bem-vindo(a), %s!", nome);
+		System.out.printf("Bem-vindo(a), %s ! ", nome);
 		System.out.printf("%s, você tem %d anos.", nome, idade);
 		
 	}
