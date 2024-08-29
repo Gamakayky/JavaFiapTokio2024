d36ab3c157d9e33326b9455e092cf5a800c84c95
Gamakayky <kaykyalexandre18@icloud.com>
2024-08-29 17:53:47 -0300

    att exercicio aula05

---
+++ JavaTokio2024/src/logica/exercicios/aula05/Exercicio08.java
@@ -0,0 +1,32 @@
+package logica.exercicios.aula05;
+
+import java.util.Scanner;
+
+public class Exercicio08 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Montar um script onde o usuário entre com o valor em Reais e faça a sua respectiva
+conversão para:
+▪ Dólar Americano – Euro - Peso Argentino - Libra Esterlina - Iene
+▪ Exibir na tela todos os valores obtidos com apenas 02 casas decimais.
+▪ Valor em real:
+▪ Valor em dólar:
+▪ Valor em euro:
+▪ ...
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o valor em reais: ");
+		double reais = sc.nextDouble()
+;
+		
+		System.out.println("valor em real: " + reais);
+		
+		double dolarAmericano = reais / 5.64;
+		System.out.println("valor em dolar: " +dolarAmericano);
+	}
+
+}
